7ccb90722c9a9c2892e0464f406ac45192ec1781
Dave Geddes <davidcgeddes@gmail.com>
2011-08-10 17:23:14 -0600

    picked a simple API

--- cinch.js
+++ cinch.js
@@ -1,3 +1,30 @@
-define(['jQuery'], function(){
-    console.log('cinch.js started');
+define(['jQuery'], function() {
+	/**
+	 * cinch creates or adds to a controller
+	 * @param model
+	 * @param view
+	 */
+	function cinch(model, view) {
+		//var shared
+		
+		return {
+			view: view,
+			model: model,
+			to: function(controller){
+				console.log('cinching model and view to the controller');
+			}
+		}
+	}
+
+	/**
+	 * grip creates a view out of the populated HTML
+	 * @param populatedHTML
+	 */
+	cinch.grip = function(populatedHTML) {
+		console.log('you wants grips?');
+		//todo create grips
+		return $(populatedHTML);
+	};
+
+	return cinch;
 });
--- test/cinchjs-test.js
+++ test/cinchjs-test.js
@@ -1,24 +1,33 @@
 require({
-    paths: {
-        'cinch': '../cinch',
-        'text': "deps/text",
-        'jQuery' : "deps/jquery-1.6.2"
-    }
+	paths: {
+		'cinch': '../cinch',
+		'text': "lib/text",
+		'jQuery' : "lib/jquery-1.6.2"
+	}
 });
 
-require(['cinch', 'jQuery', 'deps/qunit'], function(cinch) {
-    
-    //setup/takedown
-    module("cinch.js", {
-        setup: function(){
+require(['cinch', 'jQuery', 'lib/qunit', 'lib/handlebars'], function(cinch) {
 
-        },
-        teardown: function(){
-            
-        }
-    });
+	//setup/takedown
+	module("cinch.js", {
+		setup: function() {
+			
+		},
+		teardown: function() {
 
-    test('tests setup', function(){
-      ok(true);
-    })
+		}
+	});
+
+	test("create grips on the interesting parts of the populated template", function(){
+		var simple = "<div data-grip='message'>{{message}}</div>";
+		var simpleTemplate = Handlebars.compile(simple);
+		var model = {
+			message: "Hi There"
+		};
+		var populated = simpleTemplate(model);
+		console.log('populated: ', populated);
+		var view = cinch.grip(populated);
+		view.addClass('1');
+		cinch(model, view).to(this);
+	});
 });
